cc7c2fb73d6d2c5804dd4d7581b852e903e5730a
mjczyz <mike@viziblee.com>
2016-03-15 16:45:10 -0500

    Update verify.php

--- flex_form/verify.php
+++ flex_form/verify.php
@@ -8,7 +8,7 @@ if ($_COOKIE[login] != "")
 {
 //$q = mysql_query("select * from users WHERE id = '$_COOKIE[login]';");
 
-$q = mssql_query("SELECT tid FROM listdb.dbo.vwwebaccess where tid = '$_COOKIE[login]';");
+$q = mssql_query("SELECT tid FROM ### where tid = '$_COOKIE[login]';");
 while($f=mssql_fetch_array($q))
 {
 if ($f[tid] != $_COOKIE[login])
